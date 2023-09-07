5ca11968fe08ee969b76417ebfee08b8cb50c44e
giovannimhern <85322117+giovannimhern@users.noreply.github.com>
2023-09-06 19:47:47 -0700

    Mobile edits

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const about = <div className='md:grid md:grid-cols-2 flex flex-col justify-cente
 
 const impactSection = <div className='px-7'>
     <h2 className='flex justify-center items-center capitalize font-bold text-xl md:text-6xl pb-5'>my impact in numbers</h2>
-    <div className='md:px-20  px-8 flex flex-col md:grid md:grid-cols-3 md:gap-7'>
+    <div className='md:px-5 flex flex-col md:grid md:grid-cols-3 md:gap-[50px]'>
         {card(<a href='#'><svg className="hover:fill-blue-300 fill-white h-8 w-8 md:h-10 md:w-10" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 448 512"><path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z" /></svg></a>, 'Tiktok', '4M+ Views', '18K+ Followers')}
         {card(<a href='#'><svg className="hover:fill-blue-300 fill-white h-8 w-8 md:h-10 md:w-10" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" /></svg></a>, 'Youtube', '200K+ Views', '1.6K+ Followers')}
         {card(<a href='#'><svg className="hover:fill-blue-300 fill-white h-8 w-8 md:h-10 md:w-10" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" /></svg></a>, 'LinkedIn', '400 Subs', '2.2K+ Followers')}
@@ -72,11 +72,11 @@ const impactSection = <div className='px-7'>
 
 
 function card(tag: string | number | boolean | JSX.Element | Iterable<ReactNode> | PromiseLikeOfReactNode | null | undefined, title: string, views: string, followers: string) {
-    return <div className='rounded-xl py-2 px-10'>
+    return <div className='rounded-xl py-2 px-10 border border-white'>
         <div className='flex gap-x-6 justify-center'>
             {tag}
         </div>
-        <div className='flex gap-10 pt-5 justify-center items-center'>
+        <div className='flex gap-x-5 pt-5 justify-center'>
             <p className='lg:text-lg md:text-md text-xs font-md'>{views}</p>
             <p className='lg:text-lg md:text-md  text-xs font-md'>{followers}</p>
         </div>
