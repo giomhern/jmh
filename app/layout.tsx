86c1b44d5cff261aaedbb7afe17aa892c6d8110e
giovannimhern <giovannimhern@uchicago.edu>
2024-03-09 22:35:12 -0600

    Subtle changes for bg mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + "bg-blue-600 min-h-screen"}>
+      <body className={inter.className}>
         <Navbar />
         {children}
         <Analytics />
