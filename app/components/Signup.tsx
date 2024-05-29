f7c481e5860e610b5cf58e576c6130ae20cc5ffd
giovannimhern <giovannimhern@uchicago.edu>
2024-05-28 23:27:58 -0700

    Sign up design changes

--- app/components/Signup.tsx
+++ app/components/Signup.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 export default function Signup() {
   return (
-    <form className="gap-5 border-2 border-gray-800 py-2 px-3 flex items-center">
+    <form className="md:gap-5 gap-1 border-2 border-gray-800 py-2 px-3 flex items-center">
       <Image src="/email.png" width={32} height={32} alt="Subscribe" />
       <input
         type="email"
         placeholder="Your Email Address"
         className="p-2 font-regular md:text-lg text-sm focus:ring-0 focus:ring-offset-0"
       />
-      <button className=" text-white md:text-lg text-sm py-2 px-6 font-medium border border-transparent bg-gray-800">
+      <button className=" text-white md:text-lg text-xs py-2 md:px-6 px-4 font-medium border border-transparent bg-gray-800">
         Subscribe
       </button>
     </form>
--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -1,14 +1,14 @@
 import impressions from "./ImpressionsData";
 export default function Socials() {
   return (
-    <section className="bg-blue-100 py-24 flex flex-col md:rounded-t-[125px] rounded-t-[75px]">
+    <section className="py-12 flex flex-col">
       <p className="font-semibold tracking-tight leading-[1.5] mx-auto text-gray-800 break-words leading-regular md:text-4xl text-xl text-center pb-10">
         Utilize social networks to create meaningful impact.
       </p>
 
       <div className="relative flex px-10 pt-5 pb-10 items-center">
         <div className="flex-grow border-t border-gray-800"></div>
-        <div className="flex-shrink px-4 py-2 relative top-1 -rotate-[12deg] bg-white bottom-5 rounded-full">
+        <div className="flex-shrink px-4 py-2 relative top-1 -rotate-[12deg] bg-blue-100 bottom-5 rounded-full">
           <p className="md:text-lg text-sm tracking-tight text-black">
             Social Media Reach
           </p>
